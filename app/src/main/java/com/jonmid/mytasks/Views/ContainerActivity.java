72e1688579ff6256ee4bbc07fc19499d7232fe17
LeandAyala <leandroayala883@gmail.com>
2017-05-10 13:53:02 -0500

    Fragments

---
+++ app/src/main/java/com/jonmid/mytasks/Views/ContainerActivity.java
@@ -0,0 +1,60 @@
+package com.jonmid.mytasks.Views;
+
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jonmid.mytasks.R;
+import com.jonmid.mytasks.Views.Fragments.HomeFragment;
+import com.jonmid.mytasks.Views.Fragments.ProfileFragment;
+import com.jonmid.mytasks.Views.Fragments.SearchFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.id_bottombar);
+        bottomBar.setDefaultTab(R.id.tab_home);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.tab_search:
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.id_container_fragment , searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.tab_home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.id_container_fragment , homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.tab_profile:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.id_container_fragment , profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+
+                }
+            }
+        });
+    }
+}
